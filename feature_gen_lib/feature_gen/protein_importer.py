e4d33233866aa804d94a691c0f8a9e51a9cb7942
edwardbirdlab <edwardbirdlab@gmail.com>
2024-09-24 16:18:29 -0500

    function to import PROKKA aa's

---
+++ feature_gen_lib/feature_gen/protein_importer.py
@@ -0,0 +1,11 @@
+from Bio import SeqIO
+
+def read_protein_fasta(fasta):
+    """
+    Function that imports a protein fasta file into a dictionary
+    """
+    with open(fasta, "r") as fasta_file:
+        fasta_dict = {}
+        for record in SeqIO.parse(fasta_file, "fasta"):
+            fasta_dict[record.id] = str(record.seq)  # Convert sequence to a string if needed
+    return fasta_dict
